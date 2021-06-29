345b60e5bab9d845965720e02ef61854b91baf0e
Sören Tempel <tempel@uni-bremen.de>
2021-06-29 17:04:46 +0200

    sw: adjust example code

--- sw/basic-c/main.c
+++ sw/basic-c/main.c
@@ -4,14 +4,15 @@
 extern void make_symbolic(volatile void *, size_t);
 
 int main() {
-	char buf[3];
+	char buf[3] = {0, 42, 5};
 	unsigned int a;
-	make_symbolic(&a, sizeof(a));
 
-	if (a < 3) {
-		a++;
-	} else {
-		a--;
+	make_symbolic(&a, sizeof(a));
+	if (a < sizeof(buf)) {
+		if (buf[a] > 10) {
+			buf[a] = 23;
+			asm("EBREAK");
+		}
 	}
 
 	return 0;
